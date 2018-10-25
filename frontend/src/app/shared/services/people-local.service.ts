afcdd38edba8831fd68d64374cdb5a272449253c
michaeldeloris <michaeldeldev@gmail.com>
2018-10-25 09:46:00 +0200

    Changes commited for working with backend removed

--- frontend/src/app/shared/services/people-local.service.ts
+++ frontend/src/app/shared/services/people-local.service.ts
@@ -3,7 +3,7 @@ import { LocalStorage } from '@ngx-pwa/local-storage';
 import { People } from '../models/people.model';
 import { Observable, of } from 'rxjs';
 import { VerificationsService } from './verifications.service';
-import { GeneratorService } from './generator.service'
+import { GeneratorService } from './generator.service';
 import { WikiApiService } from './wiki-api.service';
 import { SnackbarService } from '../services/snackbar.service';
 import { HttpClient, HttpResponse } from '@angular/common/http';
@@ -13,7 +13,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 })
 export class PeopleLocalService {
 
-  uri = "http://localhost:4000";
+  uri = "http://localhost:3000";
 
   constructor(
     protected localStorage: LocalStorage,
@@ -26,51 +26,40 @@ export class PeopleLocalService {
 
   storeOnLocalStorage(newPeople: People): any {
 
-    // var people: People[] = JSON.parse(localStorage.getItem('peoples') || "[]");           //Name
-    var checkName: boolean = this.verification.isUniqueName(newPeople.name);
+    var people: People[] = JSON.parse(localStorage.getItem('peoples') || "[]");
+    var checkName = this.verification.isUniqueName(newPeople.name);
     if (!checkName) {
-      this.snackBar.existingPeople();
+
+      alert('People already exists!');
       return;
     }
-    newPeople.id = this.generator.generateId();                                           //Id
-    
-    // this.wikiApi.getExtract(newPeople.name).subscribe(                                    //Resume
-    //   res => {
-    //     var resultString = JSON.stringify(res.body);
-    //     var resultParsed = JSON.parse(resultString);
-    //     const pageId = Object.keys(resultParsed.query.pages)[0];
-    //     const verif = this.verification.isMatchingId(pageId);
-    //     const result = resultParsed.query.pages[pageId].extract;
-    //     newPeople.resume = result;
-    //     if (res && verif == true) {
+    newPeople.id = this.generator.generateId();
 
-    //       this.snackBar.addPeople();
-          
-    //     } else {
-    //       this.snackBar.addPeopleFail();
-    //     }
-    //   });
-
-      // return this.http.post(`${this.uri}/peoples/add`, newPeople);
-
-      const people = {
-        id : newPeople.id,
-        name: newPeople.name,
-        resume: 'test'
-      }
-
-      return this.http.post(`${this.uri}/peoples/add`, people);
-
+    this.wikiApi.getExtract(newPeople.name).subscribe(
+      res => {
+        var resultString = JSON.stringify(res.body);
+        var resultParsed = JSON.parse(resultString);
+        const pageId = Object.keys(resultParsed.query.pages)[0];
+        const verif = this.verification.isMatchingId(pageId);
+        const result = resultParsed.query.pages[pageId].extract;
+        newPeople.resume = result;
+        if (res && verif == true) {
+          people.push(newPeople);
+          localStorage.setItem('peoples', JSON.stringify(people));
+          this.snackBar.addPeople();
+        }else{
+          this.snackBar.addPeopleFail();
+        }
+      });
   }
 
   getPeople() {
-    return this.http.get(`${this.uri}/peoples`);
 
-    // var people: People[] = JSON.parse(localStorage.getItem('peoples') || "[]");
-    // return of(people);
-  }
+    var people: People[] = JSON.parse(localStorage.getItem('peoples') || "[]");
+    return of(people);
+  };
 
-  deletePeople(peopleId): void {
+  deletePeople(peopleId): any {
 
     var people: People[] = JSON.parse(localStorage.getItem('peoples') || "[]");
     var peopleindex = people.findIndex(People => People.id === peopleId);
@@ -80,5 +69,5 @@ export class PeopleLocalService {
       localStorage.setItem('peoples', JSON.stringify(people));
     }
     location.reload();
-  }
+   }
 }
